d9196a84ba23dbce01e8356583b09227ab979e64
Sahil Rajput <sahilrajput03@gmail.com>
2025-07-12 05:00:00 +0530

    Add script usage of a.js over HTTP server.

---
+++ scripts/a.js
@@ -0,0 +1,9 @@
+// 🚀 Steps to use this script via a HTTP server in your wordpress website [Teste
+//      with my Docker Setup]:
+// 1. Start the server via --- serve -l 8081
+// 2. To insert this script in your wordpress, simply add WPCode plugin and add
+//    a "Add Your Custom Code (New Snippet)" snippet with following code: 
+//      <script src="http://localhost:8081/scripts/a.js" > </script>
+// 3. Make sure you turn on the toggle on to activate and click on "Update" button.
+
+alert(987);
