f58d363f61d3673ca524e386dfa4346bbb7e780b
Sahil Rajput <sahilrajput03@gmail.com>
2025-07-12 05:06:49 +0530

    Update.

--- scripts/a.js
+++ scripts/a.js
@@ -3,7 +3,7 @@
 // 1. Start the server via --- serve -l 8081 --cors
 // 2. To insert this script in your wordpress, simply add WPCode plugin and add
 //    a "Add Your Custom Code (New Snippet)" snippet with following code: 
-//      <script src="http://localhost:8081/scripts/a.js" > </script>
+//      <script type="module" src="http://localhost:8081/scripts/a.js" > </script>
 // 3. Make sure you turn on the toggle on to activate and click on "Update" button.
 // - For production usage - you can make use of files hosted on github.com/myrepo/myfile.js
 
